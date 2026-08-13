28f10795fe8e1f8d0553d91cdcf00c9435a96aa0
youss-ef-69 <youssef.2002.zedan@gmail.com>
2026-08-13 13:04:44 +0300

    v2.0.0

--- modules/__ini__.py
+++ modules/__ini__.py
@@ -1,7 +1,22 @@
-# Z-Storm Modules
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Z-Storm Modules Package
+"""
+
 from .smart_scanner import SmartScanner
 from .advanced_report import AdvancedReport
 from .arp_spoof import ARPSpoofer
 from .auto_lab import AutoLab
+from .dtp_spoof import DTPSpoofer
 
-__all__ = ['SmartScanner', 'AdvancedReport', 'ARPSpoofer', 'AutoLab']
+__all__ = [
+    'SmartScanner',
+    'AdvancedReport',
+    'ARPSpoofer',
+    'AutoLab',
+    'DTPSpoofer'
+]
+
+__version__ = '2.0.0'
